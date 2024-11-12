c21f6c0a8be136b2c774ab38c64f543fcf375b98
Ilyas KHIAT <kiatdotblend@gmail.com>
2024-11-12 13:09:22 +0100

    test

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,4 +1,6 @@
-function verify_passworrd(password) {
+
+
+function verify_passworrd(password,length_limit = 8,) {
     // Password must be at least 8 characters long
     if (password.length < 8) {
         return false;
@@ -9,7 +11,10 @@ function verify_passworrd(password) {
         return false;
     }
 
-    
+    // Password must contain at least one letter
+    if (!/[a-zA-Z]/.test(password)) {
+        return false;
+    }
 
     return true;
 }
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -2,11 +2,15 @@ const passwordValidator = require('./passwordValidator');
 
 test('password length is less than 8 characters', () => {
     const result = passwordValidator('abc');
-    expect(result).toStrictEqual(false);
+    expect(result).toBe(false); 
 });
 
 test('password does not contain a number', () => {
     const result = passwordValidator('abcdefgh');
-    expect(result).toStrictEqual(false);
+    expect(result).toBe(false);
 });
 
+test('password does not contain a letter', () => {
+    const result = passwordValidator('12345678');
+    expect(result).toBe(false);
+});
